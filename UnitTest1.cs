86704d2e709d6c0180f80db2947cd47c61cc33ec
Pavezlo <Pavelzlo18@yandex.ru>
2022-02-10 22:38:11 +0300

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace addressbook-web-tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
